74661e9ac36641c26a68220a86cb8a8bdc7c6a6b
Voidarchive <voidarchived@gmail.com>
2022-09-08 21:55:23 +0545

    footer section responsive with copyright section

--- src/routes/_footer.svelte
+++ src/routes/_footer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	let currentYear = new Date().getFullYear();
 	interface Link {
 		alt: string;
 		url?: string;
@@ -29,52 +30,65 @@
 	];
 </script>
 
-<!-- Email -->
-<div class="w-10 fixed bottom-0 left-auto right-6 z-10 ">
-	<div class="line">
-		<a
-			class="mx-auto my-5 text-nord4 hover:text-nord11 text-sm scale-y-95 hover:scale-y-100 ease-in duration-300"
-			href="mailto:anishshresthacode@gmail.com"
-			>anishshresthacode@gmail.com
-		</a>
-	</div>
-</div>
-
-<!-- Social Link -->
-<div class="w-10 fixed bottom-0 right-auto left-6 z-10 ">
-	<div class="line gap-6">
-		{#each socialLinks as icon}
-			<a href={icon.url} target="_blank" rel="noopener">
-				<svg
-					class=" stroke-nord4 hover:stroke-nord11 scale-90 hover:scale-100 duration-300"
-					width="24"
-					height="24"
-				>
-					<use xlink:href={icon.id} href={icon.id} />
-				</svg>
+<footer>
+	<!-- Email -->
+	<div class="hidden md:block w-10 fixed bottom-0 left-auto right-4 z-10 ">
+		<div class="line">
+			<a
+				class="text-vertical mx-auto my-5 text-nord4 hover:text-nord11 text-sm scale-y-95 hover:scale-y-100 ease-in duration-300"
+				href="mailto:anishshresthacode@gmail.com"
+				>anishshresthacode@gmail.com
 			</a>
-		{/each}
+		</div>
 	</div>
-</div>
+
+	<!-- Social Link -->
+	<div class="md:w-10 md:fixed bottom-0 md:right-auto md:left-4 md:z-10">
+		<div class="line gap-6 justify-center">
+			{#each socialLinks as icon}
+				<a href={icon.url} target="_blank" rel="noopener">
+					<svg
+						class=" stroke-nord4 hover:stroke-nord11 scale-90 hover:scale-100 duration-300"
+						width="24"
+						height="24"
+					>
+						<use xlink:href={icon.id} href={icon.id} />
+					</svg>
+				</a>
+			{/each}
+		</div>
+	</div>
+
+	<!-- Copyright div -->
+	<div class="text-nord4 text-center p-4 text-xs">
+		© {currentYear} Copyright: Anish shrestha.
+	</div>
+</footer>
 
 <style>
-	a {
-		writing-mode: vertical-rl;
-	}
 	.line {
 		display: flex;
-		flex-direction: column;
-		-webkit-box-align: center;
-		align-items: center;
-		position: relative;
+		gap: 1rem;
 	}
-	.line::after {
-		content: '';
-		display: block;
-		width: 1px;
-		height: 90px;
-		margin: 0px auto;
-		--tw-text-opacity: 1;
-		background-color: rgb(216 222 233 / var(--tw-text-opacity));
+	@media (min-width: 48em) {
+		.text-vertical {
+			writing-mode: vertical-rl;
+		}
+		.line {
+			flex-direction: column;
+			-webkit-box-align: center;
+			align-items: center;
+			position: relative;
+			gap: 1.5rem;
+		}
+		.line::after {
+			content: '';
+			display: block;
+			width: 1px;
+			height: 90px;
+			margin: 0px auto;
+			--tw-text-opacity: 1;
+			background-color: rgb(216 222 233 / var(--tw-text-opacity));
+		}
 	}
 </style>
