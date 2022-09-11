4b4e02132fb777e41c599aa828b92d9d84394f66
Voidarchive <voidarchived@gmail.com>
2022-09-11 20:36:56 +0545

    fade transition on initial load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,23 @@
 	import Footer from '$lib/main/_footer.svelte';
 	import { fade } from 'svelte/transition';
 	import type { LayoutData } from './$types';
-
+	import { onMount } from 'svelte';
+	let load = false;
+	onMount(() => {
+		load = true;
+	});
 	export let data: LayoutData;
 </script>
 
 <div class="bg-nord0 h-full">
-	<Header />
+	{#if load}
+		<Header />
 
-	{#key data.currentRoute}
-		<main in:fade={{ duration: 2000 }} out:fade={{ duration: 150 }}>
-			<slot />
-		</main>
-	{/key}<Footer />
+		{#key data.currentRoute}
+			<main in:fade={{ duration: 2000 }} out:fade={{ duration: 150 }}>
+				<slot />
+			</main>
+		{/key}
+		<Footer />
+	{/if}
 </div>
