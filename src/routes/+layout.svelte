298dfc0e4e72f635261e43df8ec2af3f83179666
Voidarchive <voidarchived@gmail.com>
2022-10-06 13:05:23 +0545

    remove on mount page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,23 +5,16 @@
 	import Footer from '$lib/main/_footer.svelte';
 	import { fade } from 'svelte/transition';
 	import type { LayoutData } from './$types';
-	import { onMount } from 'svelte';
 
-	let load = false;
-	onMount(() => {
-		load = true;
-	});
 	export let data: LayoutData;
 </script>
 
 <div class="bg-nord0 h-full">
-	{#if load}
-		{#key data.currentRoute}
-			<Header />
-			<main in:fade={{ duration: 300 }} out:fade={{ duration: 150 }}>
-				<slot />
-			</main>
-			<Footer />
-		{/key}
-	{/if}
+	{#key data.currentRoute}
+		<Header />
+		<main in:fade={{ duration: 150 }} out:fade={{ duration: 150 }}>
+			<slot />
+		</main>
+		<Footer />
+	{/key}
 </div>
